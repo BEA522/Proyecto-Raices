d8e5df12dd04bd8f22db406a15dacca2ef8365f6
Bea <bea.loforte@gmail.com>
2025-05-17 14:02:27 -0300

    Modificacion

--- front/src/components/novedades/NovedadItem.js
+++ front/src/components/novedades/NovedadItem.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const NovedadItem = (props) => {
-    const { title, subtitle, imagen, body } = props;
-
+const NovedadItem = ({ title, subtitle, imagen, body }) => {
     return (
         <div className="novedades">
             <h1>{title}</h1>
             <h2>{subtitle}</h2>
-            <img src={imagen} />
+            <img src={imagen} alt={title} />
             <div dangerouslySetInnerHTML={{ __html: body }} />
             <hr />
         </div>
--- front/src/pages/NovedadesPage.js
+++ front/src/pages/NovedadesPage.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import NovedadItem from '../components/novedades/NovedadItem';
 import "../styles/layout.css";
 
-const NovedadesPage = (props) => {
+const NovedadesPage = () => {
 
     const [loading, setLoading] = useState(false);
     const [novedades, setNovedades] = useState([]);
@@ -11,8 +11,16 @@ const NovedadesPage = (props) => {
     useEffect(() => {
         const cargarNovedades = async () => {
             setLoading(true);
-            const response = await axios.get('http://localhost:3001/api/novedades');
-            setNovedades(response.data.novedades);
+            try { 
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            if (response.data.succes) {
+                setNovedades(response.data.novedades);
+            } else {
+                console.error('Error en la API:', response.data.message);
+            }
+        } catch (error) {
+            console.error('Error fetching novedades:', error);
+        }
             setLoading(false);
         };
 
